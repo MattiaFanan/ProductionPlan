c1b70aa7d582249b56cf72486b3567dcdff07462
MattiaFanan <mattia.fanan@gmail.com>
2020-07-05 12:02:26 +0200

    changed from single build_model to every step build_model cause slots number changes

--- Timing.py
+++ Timing.py
@@ -4,22 +4,26 @@ from PPBase import buildmodel as build_base_model
 from PPMulltyCommodity import buildmodel as build_m_c_model
 
 opt = SolverFactory('cplex_persistent')
-base_model = build_base_model()
-m_c_model = build_m_c_model()
+
+# todo extract build_model from timed code --> it's required to rebuild the model because number of slots changes
 
 
 def base():
+    base_model = build_base_model()
     instance = base_model.create_instance()
     opt.set_instance(instance)
     return opt.solve(tee=False)
 
 
 def multi_com():
+    m_c_model = build_m_c_model()
     instance = m_c_model.create_instance()
     opt.set_instance(instance)
     return opt.solve(tee=False)
 
 
 if __name__ == "__main__":
-    print(timeit(stmt=base, number=1))
-    print(timeit(stmt=multi_com, number=1))
+    for i in range(10):
+        print(timeit(stmt=base, number=1))
+        print(timeit(stmt=multi_com, number=1))
+        print("###")
