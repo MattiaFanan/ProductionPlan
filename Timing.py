b019e9c7e87fee07be30e40d62c6ce13593b7f27
MattiaFanan <mattia.fanan@gmail.com>
2020-07-21 16:40:58 +0200

    refactored Timing.py -> added modified_repeat

--- Timing.py
+++ Timing.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from pyomo.opt import SolverFactory
-from timeit import repeat
+from timeit import timeit
 from PPBase import PPBase
 from PPMultiCommodity import PPMultiCommodity
 from ParamGenerator import ParamGenerator
@@ -11,20 +11,30 @@ base_abstract_model = PPBase(ParamGenerator())
 multi_commodity_abstract_model = PPMultiCommodity(ParamGenerator())
 
 
-def solve_multi_commodity():
+def modified_repeat(untimed_pre_stmt, timed_stmt, repeat):
+    executions_array = np.zeros(repeat)
+    for i in range(repeat):
+        untimed_pre_stmt()
+        executions_array[i] = timeit(stmt=timed_stmt, number=1)
+    return executions_array
+
+
+def solve():
+    opt.solve(tee=False)
+
+
+def init_multi_commodity():
     multi_commodity_instance = multi_commodity_model.create_instance()
     opt.set_instance(multi_commodity_instance)
-    opt.solve(tee=False)
 
 
-def solve_base():
+def init_base():
     base_instance = base_model.create_instance()
     opt.set_instance(base_instance)
-    opt.solve(tee=False)
 
 
 if __name__ == "__main__":
-    reps = 1
+    reps = 100
     x = np.arange(10, 110, 10)
 
     y_base = np.zeros(10)
@@ -35,8 +45,10 @@ if __name__ == "__main__":
         base_model = base_abstract_model.build_model()
         multi_commodity_model = multi_commodity_abstract_model.build_model()
         # creates new instance with same number of production slots and solves it --> for #reps times
-        y_base[i] = np.average(repeat(stmt=solve_base, repeat=reps, number=1))
-        y_multi_commodity[i] = np.average(repeat(stmt=solve_multi_commodity, repeat=reps, number=1))
+        y_base[i] = np.average(
+            modified_repeat(untimed_pre_stmt=init_base, timed_stmt=solve, repeat=reps))
+        y_multi_commodity[i] = np.average(
+            modified_repeat(untimed_pre_stmt=init_multi_commodity, timed_stmt=solve, repeat=reps))
 
     plt.plot(x, y_base, label="base")
     plt.plot(x, y_multi_commodity, label="multi commodity")
@@ -45,3 +57,6 @@ if __name__ == "__main__":
     plt.ylabel("solving time")
     plt.legend(loc="upper left")
     plt.show()
+
+
+
